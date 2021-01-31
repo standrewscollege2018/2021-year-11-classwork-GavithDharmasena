bc59ac49eded3a2461d03d25c0d8e83d0e5ebc5b
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-01 09:21:19 +1300

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,10 @@
+# this program demonstrates storing information in variables
+
+# Here is a variable called name which is storing the text "Jim Poggers"
+name = "Jim Poggers"
+
+print ("Hello", name)
+
+# Here is a variable storing an integer
+num1 = 27
+print (num1 * 2)
