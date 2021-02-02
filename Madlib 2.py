4a0fb55a8240de75dab8a4755c4f366b49adde46
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-02 14:25:17 +1300

    Update Madlib 2.py

--- Madlib 2.py
+++ Madlib 2.py
@@ -1,2 +1,8 @@
-# getting variables via inputs
+# getting details
+name = input("Enter a name")
+weapon = input("Enter a cool weapon")
+monster = input("Enter a monster")
 
+
+# writing the madlib story with prints
+print ("Welcome to my secret lair {}, I see you have finally found the {} but even with the {} you are still no match for the {}.".format(name, weapon, weapon, monster))
