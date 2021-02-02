44a9ac30df07ed6d173ed1ee923376e0c304daf6
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-02 13:38:21 +1300

    Create Madlib.py

---
+++ Madlib.py
@@ -0,0 +1,10 @@
+# Getting the 5 variables
+name = str(input("What is your name?"))
+family1 = str(input("Who is your favourite member of your family?"))
+disease = input("What is the deadliest disease you know?")
+Lmoney = input("What would you consider a large sum of money?")
+country = input("What country do you dislike the most?")
+
+# printing the madlib
+print ("Sadly,")
+print (family1, "has fallen ill with a serious case of",disease, "and they will need professional treatment from a Doctor in",country, "and the treatment will cost: $" ,Lmoney)
