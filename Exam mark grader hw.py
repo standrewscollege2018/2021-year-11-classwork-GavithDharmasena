ad23f2c8be1616eb1effdc5cabddbfac294d913c
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-05 09:58:00 +1300

    Update Exam mark grader hw.py

--- Exam mark grader hw.py
+++ Exam mark grader hw.py
@@ -8,6 +8,7 @@ C_GRADE = 50
 #geting user's mark
 mark = int(input("What is your exam mark?: "))
 
+#printing the right grade accoriding to user's mark
 if mark >= A_GRADE:
     print("You achieved an A on your exam, well done!")
 elif mark >= B_GRADE:
@@ -17,8 +18,8 @@ elif mark >= C_GRADE:
 else:
     print("You failed your exam :(")
 
+#showing the program has ended
 print("")
 print("This is the end of the program.")
-
 
 
