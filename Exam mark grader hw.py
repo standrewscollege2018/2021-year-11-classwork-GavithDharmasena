3bc9ea19443a2dc405d6749090cab9b391640e4f
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-04 17:55:56 +1300

    Create Exam mark grader hw.py

---
+++ Exam mark grader hw.py
@@ -0,0 +1,24 @@
+#this program takes an exam mark and prints the grade accordingly
+
+#constants are the grade boundaries
+A_GRADE = 90
+B_GRADE = 70
+C_GRADE = 50
+
+#geting user's mark
+mark = int(input("What is your exam mark?: "))
+
+if mark >= A_GRADE:
+    print("You achieved an A on your exam, well done!")
+elif mark >= B_GRADE:
+    print("You achieved a B on your exam, good job!")
+elif mark >= C_GRADE:
+    print("You achieved a C on your exam")
+else:
+    print("You failed your exam :(")
+
+print("")
+print("This is the end of the program.")
+
+
+
