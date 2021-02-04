cdfb852caaec55811123eb6434c8cb2654734856
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-04 15:14:54 +1300

    Create Welcome to the zoo.py

---
+++ Welcome to the zoo.py
@@ -0,0 +1,13 @@
+#Program that asks for age of people entering the zoo and giving them the child price if they are of the right age
+
+#constant child age
+CHILD_AGE = 13
+
+age = int(input("What is your age?: "))
+
+if age < CHILD_AGE:
+    print("You pay the child price!")
+
+print ("welcome to the zoo.")
+print (" ")
+print ("This is the end of my program.")
