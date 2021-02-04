30182a50ad8a80c93b7c1150f84f67baabec119a
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-04 13:18:57 +1300

    Update Madlib.py

--- Madlib.py
+++ Madlib.py
@@ -1,3 +1,5 @@
+# Code for a madlib story
+
 # Getting the 5 variables
 name = str(input("What is your name?"))
 family1 = str(input("What is the name of your favourite family member?"))
@@ -6,5 +8,5 @@ Large_money = input("What would you consider a large sum of money?")
 country = input("What country do you dislike the most?")
 
 # printing the madlib with the variables
-print ("Sadly,")
-print (family1, "has fallen ill with a serious case of",disease, "and they will need professional treatment from a Doctor in",country, "and the treatment will cost: $" ,Large_money)
+print ("Dear {},".format(name))
+print ("Sadly, " + family1, "has fallen ill with a serious case of",disease, "and they will need professional treatment from a Doctor in ",country, "and the treatment will cost:" ,Large_money)
