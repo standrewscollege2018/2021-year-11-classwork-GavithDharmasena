7edc704c60e2d9d131f8f774304fc2517cda733f
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-16 14:26:24 +1300

    guesser 1 to 100

--- while loop 1 to 10 guesser.py
+++ while loop 1 to 10 guesser.py
@@ -6,25 +6,40 @@ import random
 #constants
 random_num = random.randint(1,100)
 keep_guessing = True
+play_again = True
 count = 0
+highscore = 0
+new_highscore = 101
 
 #guessing
-while keep_guessing == True:
-    count += 1
-    guess = int(input("Guess a number from 1 to 100: "))
-    #showing guess is right
-    if guess == random_num:
-        print ("That's right!")
-        #showing number of guesses
-        if count == 1:
-            print ("It only took you 1 guess")
+while play_again == True:
+    while keep_guessing == True:
+        count += 1
+        guess = int(input("Guess a number from 1 to 100: "))
+        #showing guess is right
+        if guess == random_num:
+            print ("That's right!")
+            #showing number of guesses
+            if count == 1:
+                print ("It only took you 1 guess")
+            else:
+                print ("It only took you",count,"guesses.")
+                highscore = 101 - count
+                if new_highscore >= highscore:
+                    new_highscore = highscore
+                    print ("Your highscore is",str(new_highscore),"/ 100")
+                else:
+                    print ("Your highscore is",str(highscore),"/ 100")
+            #playing the game again
+            play_again = input("Would you like to play again (yes or no)?: ")
+            if play_again == "no":
+                print("Ok, thanks for playing.")
+                keep_guessing = False
+        #higher/lower
         else:
-            print ("It only took you",count,"guesses.")
-        keep_guessing = False
-    #higher/lower
-    else:
-        if random_num > guess:
-            print("higher")
-        else:
-            print("lower")
+            if random_num > guess:
+                print("higher")
+            else:
+                print("lower")
+
 
