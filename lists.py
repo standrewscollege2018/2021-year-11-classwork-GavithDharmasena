b32ea0d04e1e11e529381950197449934e70c068
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-18 15:21:06 +1300

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+#program that i use to learn lists
+
+#making a list
+my_list = ["yeet","cringe","bonk"]
+#adding something to the list (on the end)
+my_list.append("thing")
+#print the last the thing in the list (the first thing is [0])
+print (my_list[-1])
+
+#adding a string to a specific position in the list
+my_list.insert(0,"kid")
+#printing the new first thing in the list
+print (my_list[0])
