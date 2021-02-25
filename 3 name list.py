f3e8712464f2b8b7470a01c31bbad49331f87271
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-02-25 14:33:19 +1300

    Update 3 name list.py

--- 3 name list.py
+++ 3 name list.py
@@ -1,11 +1,14 @@
 #program that takes 3 names and puts them in  list, then prints the list
 
+#constants
 names = []
 count = 0
 count2 = 0
 keep_asking = True
 
-while keep_asking == True:
+#asking how many names they want to enter, and looping for the right input
+while keep_asking == True
+    #making sure an integer is entered using try & except
     try:
         num_names = int(input("How many names do you want to enter? "))
         keep_asking = False
