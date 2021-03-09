4aa8afdad2afa0eb58e10c2dc073b7e757996020
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-03-09 14:04:55 +1300

    Update Euler Project (3).py

--- Euler Project (3).py
+++ Euler Project (3).py
@@ -1,4 +1,12 @@
+#code that finds the largest prime number in 600851475143
 prime_numbers = []
-600851475143
 
-if num % 600851475143 == 0
+n = 600851475143
+for num in range (2,775146):
+    if n % num ==0:
+        prime_numbers.append(num)
+
+
+
+print (600851475143 / prime_numbers[0], "is the largest prime number")
+
