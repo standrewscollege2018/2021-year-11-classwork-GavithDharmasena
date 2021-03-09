46d7a9d00b3d594661add21d06db19eb923a0d40
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-03-09 13:35:47 +1300

    Create Euler Project (2).py

---
+++ Euler Project (2).py
@@ -0,0 +1,18 @@
+fib = 1
+fib2 = 2
+count = 0
+total = 0
+
+while count <=4000000:
+    count = fib2
+    if count % 2 == 0:
+        total += count
+    count = fib + fib2
+    fib = fib2
+    fib2 = count
+
+print (total)
+
+
+
+
