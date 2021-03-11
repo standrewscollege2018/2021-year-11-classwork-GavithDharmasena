76acba969daca8469e37b141e3f10ed57dc9c44d
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-03-12 09:16:12 +1300

    Create Euler Project (5).py

---
+++ Euler Project (5).py
@@ -0,0 +1,4 @@
+#code that finds the smallest number that is divisible by 1 to 20 without a remainder
+for num in range():
+    if num % 20 == 0 and num % 19 == 0 and num % 18 == 0 and num % 17 == 0 and num % 16 == 0 and num % 15 == 0 and num % 14 == 0 and num % 13 == 0 and num % 12 == 0 and num % 11 == 0:
+        print(num)
