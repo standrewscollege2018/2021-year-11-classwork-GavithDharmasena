45f5b3db596c27b02c5ac256dad59c4d9711400a
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-03-12 09:16:07 +1300

    Update Euler Project (3).py

--- Euler Project (3).py
+++ Euler Project (3).py
@@ -2,11 +2,11 @@
 prime_numbers = []
 
 n = 600851475143
+
 for num in range (2,775146):
     if n % num ==0:
         prime_numbers.append(num)
+        print(num)
 
 
-
-print (600851475143 / prime_numbers[0], "is the largest prime number")
-
+print (n / prime_numbers[0], "is the largest prime number that is a factor of 600851475143.")
