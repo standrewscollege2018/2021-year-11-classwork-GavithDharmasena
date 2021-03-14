1417ec788994997f7faf67c8f7879da0026bfbb5
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2021-03-15 08:51:59 +1300

    Update Euler Project (6).py

--- Euler Project (6).py
+++ Euler Project (6).py
@@ -1,8 +1,9 @@
 #code that finds the difference between the sum of the square of the first 100 numbers and the sum of the first 100 numbers squared
 count = 0
+square_num = 0
 for num in range(1,101):
-    num = num ** num
-    count = count + num
-    print(count)
+    square_num = num ** 2
+    count = count + square_num
+    print (square_num)
 
-print(count)
+print (count)
